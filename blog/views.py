d70035b8480e0f9fe9a40eb7c62d202dcb19d03f
altynaiabd <altynaiabdyrakhmanova@gmail.com>
2023-05-29 13:27:33 +0600

    add template tags

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 from .models import *
 from .forms import *
@@ -37,7 +38,12 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(PostModel, status=PostModel.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = PostModel.published.filter(tags__in=post_tags_ids.exclude(id=post.id))
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
